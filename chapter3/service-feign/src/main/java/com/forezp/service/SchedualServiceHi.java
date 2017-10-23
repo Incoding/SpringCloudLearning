0854e8b3dc204f932d2d724df95a6282fb12a01b
kun.wang1 <kun.wang1@corp.email.com>
2017-10-23 12:13:13 +0800

    repair dependence

--- chapter3/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
+++ chapter3/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @FeignClient(value = "service-hi")
 public interface SchedualServiceHi {
+
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
