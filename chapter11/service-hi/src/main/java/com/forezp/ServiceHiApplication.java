2da58c065d84ab34531484332647115e8c8451ab
kun.wang1 <kun.wang1@corp.email.com>
2017-10-25 16:27:29 +0800

    spring cloud docker

--- chapter11/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
+++ chapter11/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 192.168.2.109为宿主机ip
+ * docker run  -itd  --add-host eureka-server:192.168.2.109  -p 8763:8763 -t forezp/service-hi
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
